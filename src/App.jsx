f93b5d02c447b79bfb5a7158f34139cd23300492
nickchan01 <nicholaschan.wx@gmail.com>
2024-06-08 18:10:22 +0800

    candidate_dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import './App.css'
-import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
-import Login from './components/Authentication/LoginPage';
-import Register from './components/Authentication/RegisterPage';
-import Dashboard from './components/Dashboard/Dashboard';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Authentication/LoginPage";
+import Register from "./components/Authentication/RegisterPage";
+import Dashboard from "./components/Dashboard/Dashboard";
+import CandidateDashboard from "./components/Candidate-Dashboard/c_dashboard.jsx";
 
 function App() {
-
   return (
-   <Router>
-    <Routes>
-      <Route path='/login' element={<Login></Login>}></Route>
-      <Route path='/register' element={<Register></Register>}></Route>
-      <Route path='/dashboard' element={<Dashboard></Dashboard>} ></Route>
-    </Routes>
-   </Router>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route
+          path="/cdashboard"
+          element={<CandidateDashboard></CandidateDashboard>}
+        ></Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import LoginPage from './components/Authentication/LoginPage.jsx'
-import Dashboard from './components/Dashboard/Dashboard.jsx'
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import LoginPage from "./components/Authentication/LoginPage.jsx";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import CandidateDashBoard from "./components/Candidate-Dashboard/c_dashboard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App></App>
   </React.StrictMode>
 );
-);
-
