3faf15d95b3b1490830757d9f41fe9f73cf778e5
ICAS03 <111299269+ICAS03@users.noreply.github.com>
2024-06-08 10:42:55 +0800

    testing_speech_to_text

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import LoginPage from './components/Authentication/LoginPage.jsx'
 import Dashboard from './components/Dashboard/Dashboard.jsx'
+import Speech_to_text from './components/Speech_to_text.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <App></App>
+  <Speech_to_text></Speech_to_text>
   </React.StrictMode>
 )
