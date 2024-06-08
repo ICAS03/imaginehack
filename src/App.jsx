75ba009ffdc41c1e73e178c596e5e4ce28ee2113
hanshinnnn <hanshinkek@gmail.com>
2024-06-08 20:18:57 +0800

    user database importing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
 import Login from './components/Authentication/LoginPage';
 import Register from './components/Authentication/RegisterPage';
 import Dashboard from './components/Dashboard/Dashboard';
+import BasicTable from './components/UserDatas/BasicTable';
+import JobUpdates from './components/Dashboard/JobUpdates';
+import UserData from './components/UserDatas/UserData';
 
 function App() {
 
@@ -12,6 +15,7 @@ function App() {
       <Route path='/login' element={<Login></Login>}></Route>
       <Route path='/register' element={<Register></Register>}></Route>
       <Route path='/dashboard' element={<Dashboard></Dashboard>} ></Route>
+      <Route path='/user-data' element={<UserData></UserData>} ></Route>
     </Routes>
    </Router>
   )
