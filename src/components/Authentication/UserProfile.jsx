3f7b39853f3f85a1057bcb3b8fe9ff448e952297
Brendan Ooi <brendanooi16@gmail.com>
2024-06-09 03:31:46 +0800

    "UI Changes"

---
+++ src/components/Authentication/UserProfile.jsx
@@ -0,0 +1,13 @@
+import "UserProfile.css";
+
+function UserProfile() {
+    return(
+        <>
+        <div className="page-contents">
+            
+        </div>
+        </>
+    )
+}
+
+export default UserProfile;
